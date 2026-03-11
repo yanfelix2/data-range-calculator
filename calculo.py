51631e09f8bd45d6e86d963d8ce1232b31678ce6
YanFelix <166544537+yanfelix2@users.noreply.github.com>
2026-03-11 14:16:04 -0300

    Update calculo.py

--- calculo.py
+++ calculo.py
@@ -1,19 +1,18 @@
 def calcular_amplitude(dados):
-    # Inicializa as variáveis com o primeiro item da lista
     maior = dados[0]
     menor = dados[0]
     
-    # Percorre cada valor da lista para encontrar os limites
+  
     for valor in dados:
         if valor > maior:
             maior = valor
         if valor < menor:
             menor = valor
             
-    # Retorna o valor da amplitude
+ 
     return maior - menor
 
-# Exemplo de uso com os dados da tabela
+
 alimentacao = [320.00, 450.00, 390.00, 510.00, 280.00, 470.00, 360.00, 420.00, 340.00, 530.00]
 transporte = [150.00, 200.00, 170.00, 220.00, 120.00, 210.00, 160.00, 190.00, 140.00, 230.00]
 
